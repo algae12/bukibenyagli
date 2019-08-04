40d03d9e117c90f6daa13746471e429c3735cd65
algae12 <yisrael97@gmail.com>
2019-08-05 00:05:51 +0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from '@react95/core/Button';
+import Icon from '@react95/core/Icon';
+import './Header.css'
+
+function Header() {
+    return  (
+        <div className="header">
+            <Button className="start-btn">
+                <Icon className="icon" name="logo"></Icon>
+                <span className="text">Start</span>
+            </Button>
+        </div>
+    )
+}
+
+export default Header;
